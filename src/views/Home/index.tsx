03e8db222cd10abd4118fd9206dc5da18eadd437
ZZP <43945674+a7650@users.noreply.github.com>
2021-02-01 20:40:17 +0800

    update

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { RouteComponentProps } from 'react-router'
 
 const Home: React.FunctionComponent<RouteComponentProps> = (props) => {
-  const a = Object.assign({}, 'a')
-  return <div>Home</div>
+  let [count, setCount] = useState(1)
+  const handleState = useCallback(() => {
+    setTimeout(() => {
+      setCount(count + 1)
+    }, 1000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  useEffect(() => {
+    handleState()
+  }, [handleState])
+  return <div>{count}</div>
 }
 
 export default Home
